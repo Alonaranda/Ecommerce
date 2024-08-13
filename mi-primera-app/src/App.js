b0c9da9ea08ae83fe1b2ceddaaa87195b6000f92
Christian Alonso <calonso@Macbook-de-Christian.local>
2024-08-13 16:30:07 -0600

    Empezando con NextJS

--- mi-primera-app/src/App.js
+++ mi-primera-app/src/App.js
@@ -2,19 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 import { HolaMundo } from './components/HolaMundo';
 import { Saludar } from './components/Saludar';
+import { useEffect, useState } from 'react';
 
 function App() {
   const edad = 29;
-
   const user = {
     name: 'Juan',
     lastName: 'Perez',
     edad: 30
   }
-
   const adiosFn = () => {
     console.log("Adios");
   }
+
+  const [stateCar, setStateCar] = useState(false);
+  const encenderApagar = () => {
+    console.log('Encender/Apagar');
+    //setStateCar(!stateCar);
+    setStateCar(prev => !prev);
+    setCount(count + 1)
+  }
+
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log('Total: ' + count);
+  }, [count])
 
   return (
     <div className="App">
@@ -37,6 +49,14 @@ function App() {
         <br/>
         <hr/>
         <Saludar userInfo={user} adiosFn={adiosFn} />
+        <br/>
+        <hr/>
+        <h1>UseState: {stateCar ? 'Encendido' : 'Apagado'}</h1>
+        <button onClick={encenderApagar}>Click UseState</button>
+        <br/>
+        <hr/>
+        <h1>UseEffect: {count}</h1>
+        <button onClick={encenderApagar}>Click UseState</button>
       </header>
     </div>
   );
